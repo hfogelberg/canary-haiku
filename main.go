28eaa8dcf7052a1943256f58daa024d360dc05f1
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-05-14 16:58:46 +0100

    Signup added

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/dgrijalva/jwt-go"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -18,7 +21,18 @@ type Haiku struct {
 	When time.Time `json:"when" bson:"when"`
 }
 
+type User struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username`
+	jwt.StandardClaims
+}
+
 var tpl *template.Template
+var hmacSampleSecret = []byte("secret")
 
 func main() {
 	session, err := mgo.Dial("mongodb://localhost:27017")
@@ -36,6 +50,7 @@ func main() {
 	http.HandleFunc("/create", create(session))
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/archive", archive(session))
+	http.HandleFunc("/signup", signup(session))
 
 	http.ListenAndServe(":3000", nil)
 }
@@ -49,9 +64,52 @@ func about(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func signup(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			log.Println("GET SIGNUP")
+			tpl, err := template.New("").ParseFiles("templates/signup.html", "templates/base.html")
+			err = tpl.ExecuteTemplate(w, "base", nil)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		} else {
+			log.Println("POST SIGNUP")
+
+			err := r.ParseForm()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			var user User
+			user.Username = r.PostFormValue("username")
+			password := []byte(r.PostFormValue("password"))
+			hashedPassword, err2 := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+			if err2 != nil {
+				log.Println(err2)
+				return
+			}
+			user.Password = string(hashedPassword)
+			log.Println(user)
+
+			session := s.Copy()
+			defer session.Close()
+
+			err = session.DB("canaryhaiku").C("users").Insert(&user)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+		}
+	}
+}
+
 func archive(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("ARCHIVE")
 		session := s.Copy()
 		defer session.Close()
 
@@ -117,7 +175,6 @@ func index(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 
 func create(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("CREATE")
 		if r.Method == "GET" {
 			tpl, err := template.New("").ParseFiles("templates/create.html", "templates/base.html")
 			err = tpl.ExecuteTemplate(w, "base", nil)
@@ -144,7 +201,8 @@ func create(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 			session := s.Copy()
 			defer session.Close()
 
-			if err := session.DB("canaryhaiku").C("verses").Insert(&haiku); err != nil {
+			err = session.DB("canaryhaiku").C("verses").Insert(&haiku)
+			if err != nil {
 				log.Println("Failed insert")
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
